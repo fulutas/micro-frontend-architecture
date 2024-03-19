faf17904d6a2177cee85cd3630fa74f3228df5af
furkan.ulutas <furkan.ulutas@next4biz.com>
2024-03-19 11:15:15 +0300

    delivery micro route fix

--- delivery/src/App.jsx
+++ delivery/src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Delivery from "./Components/Delivery"
 import "./index.scss";
 
 const App = () => {
   return <div className="border-4  border-blue-700">
-  <Routes>
-   <Route path="/" /> 
- </Routes>
- </div>
+    <Routes>
+      <Route path="/" element={<Delivery />} />
+    </Routes>
+  </div>
 };
 
 export default App;
--- delivery/src/Bootstrap.jsx
+++ delivery/src/Bootstrap.jsx
@@ -9,7 +9,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/orders">
+    <BrowserRouter basename="/delivery">
         <App />
     </BrowserRouter>
   </StrictMode>
